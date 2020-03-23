11f46015c50c273b57b815e40bab21e3a0d41867
MasonLiuChn <MasonLiuChn@gmail.com>
2020-03-23 13:14:31 +0800

    修改file provider 名字，防止与其他插件冲突

---
+++ android/src/main/java/com/flutter_webview_plugin/FlutterPluginFlutterWebViewPluginFileProvider.java
@@ -0,0 +1,10 @@
+package com.flutter_webview_plugin;
+
+import androidx.core.content.FileProvider;
+
+/**
+ * Created by liumeng on 2019/1/17.
+ */
+
+public class FlutterPluginFlutterWebViewPluginFileProvider extends FileProvider {
+}
